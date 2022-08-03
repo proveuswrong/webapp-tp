e629f118a1fc6de11878de17cc5fd16642b1053e
Ferit Tunçer <ferit@cryptolab.net>
2022-08-03 14:07:05 +0100

    make sure keys are ints

--- src/components/ui/syncStatus/index.jsx
+++ src/components/ui/syncStatus/index.jsx
@@ -3,12 +3,12 @@ import * as styles from "./index.module.scss";
 
 export default function SyncStatus({latestBlock, syncedBlock}) {
   return (<div className={styles.syncStatus}>
-    <small key={syncedBlock} style={{marginTop: "auto"}}>
+    <small key={parseInt(syncedBlock)} style={{marginTop: "auto"}}>
       Synced to block: <span
       className="blink">{syncedBlock || 'Unable to fetch.'}</span>
     </small>
     <br/>
-    <small key={latestBlock} style={{marginTop: "auto"}}>
+    <small key={parseInt(latestBlock)} style={{marginTop: "auto"}}>
       Latest block: <span
       className="blink">{latestBlock || 'Unable to fetch.'}</span>
     </small>
