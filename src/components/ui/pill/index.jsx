cfed5e8974f02d088bc0786bec650b2ab08a1891
Ferit Tunçer <ferit@cryptolab.net>
2022-08-21 19:17:31 +0300

    pill text manipulation

--- src/components/ui/pill/index.jsx
+++ src/components/ui/pill/index.jsx
@@ -1,6 +1,9 @@
-import React  from "react";
+import React from "react";
 import * as styles from "./index.module.scss";
 
 export default function Pill(props) {
-  return <span className={`${styles.pill} ${props?.modifiers} ${props.children?.toLowerCase()}`}>{props.children}</span>;
+  return <span className={`${styles.pill} ${props?.modifiers} ${props.children?.toLowerCase()}`}>{props.children?.replace(/([A-Z])/g, ' $1')
+    .replace(/^./, function (str) {
+      return str.toUpperCase();
+    })}</span>;
 }
