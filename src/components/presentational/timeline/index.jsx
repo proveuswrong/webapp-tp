bddd3928cfcd271ffcd4379668e55ddb02e54a94
gratestas <stasgrate@gmail.com>
2023-03-28 19:04:33 +0300

    refactor: remove logic from timeline component

--- src/components/presentational/timeline/index.jsx
+++ src/components/presentational/timeline/index.jsx
@@ -2,13 +2,13 @@ import * as styles from "./index.module.scss";
 import { Steps } from "antd";
 
 export default function Timeline(props) {
-  const { items, current, onChange } = props;
+  const { items, current } = props;
 
   return (
     <div className={`${styles.timelineSteps}`}>
-      <Steps current={current} onChange={onChange} labelPlacement="vertical">
-        {items.map((item, index) => (
-          <Steps.Step title={item.title} key={index} description={item.description} />
+      <Steps current={current} labelPlacement="vertical">
+        {items.map((item, _index) => (
+          <Steps.Step title={item.title} key={_index} description={item.description} />
         ))}
       </Steps>
     </div>
