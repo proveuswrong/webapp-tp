b3546c8aca9252735b603151a7ffbd7e7f693b08
Ferit Tunçer <ferit@cryptolab.net>
2023-04-04 21:59:45 +0100

    refactor: restructure env obj

--- src/data/environments.js
+++ src/data/environments.js
@@ -4,7 +4,9 @@ export default {
       "0x5": {
         contractInstances: {
           "0x0136ed2132Ec1e99046889058F67c9C2fd5FD578": {
-            subgraphEndpoint: "https://api.thegraph.com/subgraphs/name/proveuswrong/thetruthpost",
+            subgraph: {
+              endpoint: "https://api.thegraph.com/subgraphs/name/proveuswrong/thetruthpost",
+            }
           },
         },
       },
@@ -15,7 +17,9 @@ export default {
       "0x5": {
         contractInstances: {
           "0xAA51402316075798040c2322D0361383577707ba": {
-            subgraphEndpoint: "https://api.thegraph.com/subgraphs/name/gratestas/thetruthpost-test-goerli",
+            subgraph: {
+              endpoint: "https://api.thegraph.com/subgraphs/name/gratestas/thetruthpost-test-goerli",
+            }
           },
         },
       },
