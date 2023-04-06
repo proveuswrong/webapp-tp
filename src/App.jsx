29532c0f37441c1077baf72e0b38094f21621fb9
Ferit Tunçer <ferit@cryptolab.net>
2023-04-06 15:09:00 +0100

    hotfix printout env

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Layout from "./layout";
 import {Outlet} from "react-router-dom";
 import React from "react";
 
+console.log(process.env)
 export default class App extends React.Component {
   render() {
     return (
