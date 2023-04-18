07b3b3e3bcc07719a130dae22412034dda86dbdc
Ferit Tunçer <ferit@cryptolab.net>
2023-04-18 08:31:28 +0100

    try to print out netlify env

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Layout from "./layout";
 import {Outlet} from "react-router-dom";
 import React from "react";
 
-console.log(process.env)
+console.log(process.env.COMMIT_REF)
 export default class App extends React.Component {
   render() {
     return (
