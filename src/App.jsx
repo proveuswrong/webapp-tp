b075ba8a90e9ab58a21034d945530eec90440ce1
Ferit Tunçer <ferit@cryptolab.net>
2023-04-18 08:42:59 +0100

    implement the switch logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@ import Layout from "./layout";
 import {Outlet} from "react-router-dom";
 import React from "react";
 
-console.log(process.env.COMMIT_REF)
+import { config } from "dotenv";
+
+const isProd = (branchName) =>
+  branchName === 'main' || branchName.startsWith('hotfix/') || branchName.startsWith('release/') || (branchName === 'develop' && process.env.PULL_REQUEST);
+
+config({
+  path: `${isProd(process.env.HEAD) ? ".env.production" : ".env.development"}`,
+});
+
+console.debug(`Environment: ${process.env.ENV}`);
+console.debug(`Head: ${process.env.HEAD}`);
+console.debug(`Commit Ref: ${process.env.COMMIT_REF}`);
+console.debug(`Pull Request: ${process.env.PULL_REQUEST}`);
+console.debug(`Review ID: ${process.env.REVIEW_ID}`);
+
+
 export default class App extends React.Component {
   render() {
     return (
