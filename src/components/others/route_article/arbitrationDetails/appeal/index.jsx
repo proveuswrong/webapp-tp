b1b9e024b4e5470a6d2e1e50acdb770a7bd8a725
gratestas <stasgrate@gmail.com>
2023-04-27 00:30:09 +0300

    refactor: use subgraph data in 'AppealPeriod' component

--- src/components/others/route_article/arbitrationDetails/appeal/index.jsx
+++ src/components/others/route_article/arbitrationDetails/appeal/index.jsx
@@ -3,49 +3,80 @@ import { BigNumber, constants, utils } from "ethers";
 import { Radio } from "antd";
 import * as styles from "./index.module.scss";
 
+import CustomButton from "/src/components/presentational/button";
 import ProgressBar from "/src/components/presentational/progressBar";
 import { EthereumContext, getAllContributors } from "/src/data/ethereumProvider";
 import useGraphFetcher from "/src/hooks/useGraphFetcher";
 
+const OPTIONS = Object.freeze({
+  Yes: 1,
+  No: 2,
+});
+
 export default function AppealPeriod({ currentRound }) {
-  const [value, setValue] = useState(1);
-  const { chainId, accounts } = useContext(EthereumContext);
-  const onChange = (e) => setValue(e.target.value);
+  const [supportedRuling, setSupportedRuling] = useState(1);
+  const { chainId, accounts, contractInstance, ethersProvider } = useContext(EthereumContext);
   const { totalToBeRaised, raisedSoFar } = currentRound;
 
-  const fetchData = useCallback(async () => {
+  const fetchData = useCallback(() => {
     return getAllContributors(chainId);
   }, [chainId]);
-  const { data: contributors, isFetching } = useGraphFetcher(fetchData);
-  console.log({ contributors, isFetching });
 
-  const yourContributions = contributors && contributors[accounts[0]]?.contributions;
-  console.log({ currentRound });
+  const { data: contributors, isFetching } = useGraphFetcher(fetchData);
+
+  const connectedAccount = contributors && contributors.find((c) => c.id === accounts[0]);
+  console.log({ connectedAccount });
+  const onChange = (e) => setSupportedRuling(e.target.value);
+
+  const handleFundAppeal = async (amount) => {
+    try {
+      const unsignedTx = await contractInstance.populateTransaction.fundAppeal(
+        currentRound?.dispute?.id,
+        supportedRuling,
+        {
+          value: utils.parseEther(amount.toString()),
+        }
+      );
+      const tx = await ethersProvider.getSigner().sendTransaction(unsignedTx);
+      await tx.wait();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
       <div className={styles.appealPeriod}>
         <h1 className={styles.title}>Appeal Crowdfunding Status</h1>
-        <Radio.Group name="radiogroup" onChange={onChange} value={value}>
+        <Radio.Group name="radiogroup" onChange={onChange} value={supportedRuling}>
           <div className={styles.appealOptions}>
             <div className={styles.option}>
               <div className={styles.topRow}>
-                <Radio value={1}>Yes</Radio>
-                <EtherValue value={totalToBeRaised[1]} />
+                <Radio value={OPTIONS.Yes}>Yes</Radio>
+                <EtherValue value={totalToBeRaised[OPTIONS.Yes]} />
               </div>
               <ProgressBar
-                percent={getContributionByRuling(yourContributions, currentRound?.id, accounts[0], 1)}
-                success={{ percent: getRaisedSoFarPercent(raisedSoFar[1], totalToBeRaised[1]) }}
+                percent={getPercentage(raisedSoFar[OPTIONS.Yes], totalToBeRaised[OPTIONS.Yes])}
+                // success={{ percent: getPercentage(raisedSoFar[OPTIONS.Yes], totalToBeRaised[OPTIONS.Yes]) }}
+                success={{
+                  percent: getPercentage(
+                    getContributionByRuling(connectedAccount, currentRound?.id, OPTIONS.Yes),
+                    totalToBeRaised[OPTIONS.Yes]
+                  ),
+                }}
               />
             </div>
             <div className={styles.option}>
               <div className={styles.topRow}>
-                <Radio value={0}>No</Radio>
-                <EtherValue value={totalToBeRaised[2]} />
+                <Radio value={OPTIONS.No}>No</Radio>
+                <EtherValue value={totalToBeRaised[OPTIONS.No]} />
               </div>
               <ProgressBar
-                percent={getContributionByRuling(yourContributions, currentRound?.id, accounts[0], 2)}
-                success={{ percent: getRaisedSoFarPercent(raisedSoFar[2], totalToBeRaised[2]) }}
+                percent={getPercentage(
+                  getContributionByRuling(connectedAccount, currentRound?.id, OPTIONS.No),
+                  totalToBeRaised[OPTIONS.No]
+                )}
+                success={{ percent: getPercentage(raisedSoFar[OPTIONS.No], totalToBeRaised[OPTIONS.No]) }}
               />
             </div>
           </div>
@@ -54,7 +85,7 @@ export default function AppealPeriod({ currentRound }) {
           <div className={styles.colorBox} /> Your contribution
         </div>
       </div>
-      <div>Appel</div>
+      <CustomButton>Fund Appeal</CustomButton>
     </>
   );
 }
@@ -63,11 +94,17 @@ function EtherValue(props) {
   return <h2>{`${parseFloat(utils.formatUnits(props.value)).toFixed(3)} ${constants.EtherSymbol}`}</h2>;
 }
 
-const getRaisedSoFarPercent = (soFar, total) => BigNumber.from(soFar).div(BigNumber.from(total)).toNumber() * 100;
+const getPercentage = (amount, total) => {
+  amount = BigNumber.from(amount);
+  total = BigNumber.from(total);
+  return amount.mul(100).div(total).toNumber();
+};
 
-const getContributionByRuling = (contributions, roundID, contributor, ruling) => {
-  const contributionId = `${roundID}-${contributor}-${ruling}`;
-  const contribution = contributions?.filter((contribution) => contribution.id === contributionId);
-  if (!contribution) return 0;
-  return contribution.amount;
+const getContributionByRuling = (contributor, roundID, ruling) => {
+  if (!contributor || !roundID || !ruling) return 0;
+
+  const contributionId = `${roundID}-${contributor?.id}-${ruling}`;
+  const contribution = contributor?.contributions?.find((contribution) => contribution.id === contributionId);
+  console.log(`amount for ruling ${ruling} is ${contribution?.amount}`);
+  return contribution?.amount ?? 0;
 };
