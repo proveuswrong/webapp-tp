c5a369126c71574e09f7173f7241bf8e52bd94fb
gratestas <stasgrate@gmail.com>
2023-05-05 00:02:33 +0300

    refactor: add timeline icons and clean up styles

--- src/components/presentational/timeline/index.jsx
+++ src/components/presentational/timeline/index.jsx
@@ -3,12 +3,17 @@ import { Steps } from "antd";
 
 export default function Timeline(props) {
   const { items, current, completed } = props;
-
   return (
     <div className={`${styles.timelineSteps}`}>
       <Steps current={current} labelPlacement="vertical">
         {items.map((item, _index) => (
-          <Steps.Step title={item.title} key={_index} description={item.description} status={completed && "finish"} />
+          <Steps.Step
+            title={item.title}
+            key={_index}
+            description={item.description}
+            icon={item.icon}
+            status={completed && "finish"}
+          />
         ))}
       </Steps>
     </div>
