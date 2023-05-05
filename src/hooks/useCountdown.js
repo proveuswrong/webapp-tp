9b2a5451d612d224c8ccfeb3758b13140431cce9
gratestas <stasgrate@gmail.com>
2023-05-05 18:15:54 +0300

    fix: stop unnecessary rerendering when timeLeft is zero

--- src/hooks/useCountdown.js
+++ src/hooks/useCountdown.js
@@ -4,24 +4,26 @@ export default function useCountdown(deadline) {
     const [timeLeft, setTimeLeft] = useState(getTimeLeft(deadline));
 
     useEffect(() => {
+        if (timeLeft.totalInSeconds === 0) return;
+
         const interval = setInterval(() => {
             setTimeLeft(getTimeLeft(deadline));
         }, 1000);
 
         return () => clearInterval(interval);
-    }, [deadline]);
+    }, [deadline, timeLeft.totalInSeconds]);
 
     return timeLeft;
 };
 
 function getTimeLeft(deadline) {
-    const totalSeconds = Math.max(Math.floor((deadline - Date.now() / 1000)), 0);
-    const days = Math.floor(totalSeconds / (3600 * 24));
-    const hours = Math.floor(totalSeconds % (3600 * 24) / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
+    const totalInSeconds = Math.max(Math.floor((deadline - Date.now() / 1000)), 0);
+    const days = Math.floor(totalInSeconds / (3600 * 24));
+    const hours = Math.floor(totalInSeconds % (3600 * 24) / 3600);
+    const minutes = Math.floor((totalInSeconds % 3600) / 60);
+    const seconds = totalInSeconds % 60;
 
-    return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds, totalInSeconds };
 };
 
 
