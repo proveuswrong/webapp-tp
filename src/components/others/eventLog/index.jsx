ae77abbfbd2c88a4b8b9cfed3c0351c96c99aa8b
gratestas <stasgrate@gmail.com>
2023-05-11 17:22:25 +0300

    refactor: apply new UI design for evidence displaying

--- src/components/others/eventLog/index.jsx
+++ src/components/others/eventLog/index.jsx
@@ -1,10 +1,120 @@
 import React, { useContext, useEffect, useState } from "react";
-import * as styles from "./index.module.scss";
-import Modal from "../../presentational/modal";
 import { constants, utils } from "ethers";
+import * as styles from "./index.module.scss";
+
+import Modal from "../../presentational/modal";
 import { getLabel } from "../../../utils/account";
 import { networkMap, EthereumContext } from "../../../data/ethereumProvider";
-import Collapse from "../../presentational/collapse";
+
+import AttachmentIcon from "jsx:/src/assets/attachment.svg";
+
+const EVENTS_TO_IGNGORE = ["Withdrawal"];
+const IPFS_GATEWAY_URL = "https://ipfs.kleros.io";
+
+export default function EventLog({ visible, onCancel, events }) {
+  const ethereumContext = useContext(EthereumContext);
+  const [evidenceDetails, setEvidenceDetails] = useState({});
+  const [expandedRows, setExpandedRows] = useState([]);
+
+  const toggleEvidenceRow = (index) => {
+    const updatedRows = [...expandedRows];
+    if (updatedRows.includes(index)) {
+      updatedRows.splice(updatedRows.indexOf(index), 1);
+    } else {
+      updatedRows.push(index);
+    }
+    setExpandedRows(updatedRows);
+  };
+
+  useEffect(() => {
+    async function fetchEvidences(ipfsPaths) {
+      try {
+        const responses = await Promise.all(ipfsPaths.map((path) => fetch(IPFS_GATEWAY_URL + path)));
+        const data = await Promise.all(responses.map((response) => response.json()));
+        const evidences = ipfsPaths.reduce((acc, path, index) => ({ ...acc, [path]: data[index] }), {});
+
+        setEvidenceDetails(evidences);
+      } catch (error) {
+        console.error("Failed to fetch event evidences from IPFS", error);
+      }
+    }
+
+    const ipfsPaths = events.filter((event) => event.name === "Evidence").map((event) => event.details);
+    if (ipfsPaths.length) fetchEvidences(ipfsPaths);
+  }, [events]);
+
+  return (
+    <Modal visible={visible} className={styles.eventLog} onCancel={onCancel} footer={null}>
+      <div className={styles.title}>Event Log</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Details</th>
+            <th>Actor</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {events
+            .filter((e) => !EVENTS_TO_IGNGORE.includes(e.name))
+            .map((event, i) => {
+              const isEvidence = event.name === "Evidence";
+              const isExpanded = expandedRows.includes(i);
+              return (
+                <React.Fragment key={`row${i}`}>
+                  <tr className={isExpanded ? styles.expanded : undefined}>
+                    <td>{getPrettyNamesForEvents(event.name)}</td>
+                    <td>
+                      {isEvidence ? (
+                        <div className={styles.expandButton} onClick={() => toggleEvidenceRow(i)}>
+                          {isExpanded ? "Hide Details" : "Show Details"}
+                        </div>
+                      ) : (
+                        formatExtraData(event.name, event.details, ethereumContext)
+                      )}
+                    </td>
+                    <td>
+                      <a
+                        href={networkMap[ethereumContext?.chainId]?.explorerURL(event.from)}
+                        target="_blank"
+                        rel="noreferrer noopener"
+                      >
+                        {getLabel(event.from, ethereumContext?.accounts[0])}
+                      </a>
+                    </td>
+                    <td>{new Date(event.timestamp * 1000).toUTCString()}</td>
+                  </tr>
+                  {isEvidence && isExpanded && (
+                    <tr>
+                      <td colSpan="4" className={styles.expanded}>
+                        <EvidenceDisplay evidence={evidenceDetails[event.details]} />
+                      </td>
+                    </tr>
+                  )}
+                </React.Fragment>
+              );
+            })}
+        </tbody>
+      </table>
+    </Modal>
+  );
+}
+
+function EvidenceDisplay({ evidence }) {
+  return (
+    <div className={styles.evidenceDisplay}>
+      <div className={styles.header}>
+        <div>{evidence.name}</div>
+        <a href={`${IPFS_GATEWAY_URL + evidence.fileURI}`} target="_blank" rel="noreferrer noopener">
+          <AttachmentIcon />
+        </a>
+      </div>
+      <hr />
+      <div className={styles.description}>{evidence.description}</div>
+    </div>
+  );
+}
 
 function getPrettyNamesForEvents(sourceCodeName) {
   switch (sourceCodeName) {
@@ -46,79 +156,4 @@ function formatExtraData(eventNameAsInSourceCode, extraData, ethereumContext) {
     default:
       return extraData;
   }
-}
-
-const EVENTS_TO_IGNGORE = ["Withdrawal"];
-const IPFS_GATEWAY_URL = "https://ipfs.kleros.io";
-
-export default function EventLog({ visible, onCancel, events }) {
-  const ethereumContext = useContext(EthereumContext);
-  const [evidenceDetails, setEvidenceDetails] = useState({});
-
-  useEffect(() => {
-    async function fetchEvidences(ipfsPaths) {
-      try {
-        const responses = await Promise.all(ipfsPaths.map((path) => fetch(IPFS_GATEWAY_URL + path)));
-        const data = await Promise.all(responses.map((response) => response.json()));
-        const evidences = ipfsPaths.reduce((acc, path, index) => ({ ...acc, [path]: data[index] }), {});
-
-        setEvidenceDetails(evidences);
-      } catch (error) {
-        console.error("Failed to fetch event evidences from IPFS", error);
-      }
-    }
-
-    const ipfsPaths = events.filter((event) => event.name === "Evidence").map((event) => event.details);
-    if (ipfsPaths.length) fetchEvidences(ipfsPaths);
-  }, [events]);
-
-  const EvidenceRow = ({ evidence }) => (
-    <td colSpan="4" className={styles.evidenceRow}>
-      <Collapse header={evidence?.name} content={evidence?.description} />
-    </td>
-  );
-
-  return (
-    <Modal visible={visible} className={styles.eventLog} onCancel={onCancel} footer={null}>
-      <div className={styles.title}>Event Log</div>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Details</th>
-            <th>Actor</th>
-            <th>Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {events
-            .filter((e) => !EVENTS_TO_IGNGORE.includes(e.name))
-            .map((event, i) => (
-              <tr key={`row${i}`}>
-                {event.name === "Evidence" ? (
-                  <EvidenceRow evidence={evidenceDetails[event.details]} />
-                ) : (
-                  <>
-                    <td>{getPrettyNamesForEvents(event.name)}</td>
-                    <td>{formatExtraData(event.name, event.details, ethereumContext)}</td>
-                    <td>
-                      {
-                        <a
-                          href={networkMap[ethereumContext?.chainId]?.explorerURL(event.from)}
-                          target="_blank"
-                          rel="noreferrer noopener"
-                        >
-                          {getLabel(event.from, ethereumContext?.accounts[0])}
-                        </a>
-                      }
-                    </td>
-                    <td>{new Date(event.timestamp * 1000).toUTCString()}</td>
-                  </>
-                )}
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    </Modal>
-  );
 }
