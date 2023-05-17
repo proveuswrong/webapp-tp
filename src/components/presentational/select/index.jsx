8b39f675c400e137d9b1e8fa8cb55f817d7f6a44
gratestas <stasgrate@gmail.com>
2023-05-17 15:50:46 +0300

    refactor: add custom Select component

--- src/components/presentational/select/index.jsx
+++ src/components/presentational/select/index.jsx
@@ -1,17 +1,50 @@
-import { Select as SelectAntDesign } from "antd";
+import { useState, useEffect, useRef } from "react";
 import * as styles from "./index.module.scss";
+
 import CaretDownOutlined from "jsx:/src/assets/caretDownOutlined.svg";
 
-export default function Select({ options, defaultValue, onSelect }) {
+export default function Select({ options, placeholder, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const selectRef = useRef(null);
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    onChange(option.value);
+    setIsOpen(false);
+  };
+
+  const handleClickOutside = (event) => {
+    if (selectRef.current && !selectRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <SelectAntDesign
-      className={styles.dropdown}
-      style={{ width: 200 }}
-      options={options}
-      defaultValue={defaultValue}
-      onSelect={onSelect}
-      suffixIcon={<CaretDownOutlined />}
-      showSearch={false}
-    />
+    <div ref={selectRef} className={`${styles.select} ${isOpen ? "open" : ""}`}>
+      <div className={styles.selectedOption} onClick={() => setIsOpen((prevState) => !prevState)}>
+        <span> {selectedOption?.label ?? placeholder}</span> <CaretDownOutlined />
+      </div>
+      {isOpen && (
+        <ul className={styles.optionList}>
+          {options.map((option) => (
+            <li
+              key={option.value}
+              className={`${styles.option} ${option.value === selectedOption?.value ? styles.selected : ""}`}
+              onClick={() => handleOptionSelect(option)}
+            >
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
