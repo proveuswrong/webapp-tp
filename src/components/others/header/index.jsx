a876cef151a9ac078d19a71dbd3e744b7511a3c1
gratestas <stasgrate@gmail.com>
2023-05-18 16:15:17 +0300

    fix: fix nav at the top of the screen when menu is opened

--- src/components/others/header/index.jsx
+++ src/components/others/header/index.jsx
@@ -1,20 +1,18 @@
-import { useContext, useEffect, useRef, useState } from "react";
 import * as styles from "./index.module.scss";
 
 import TruthPost from "jsx:/src/assets/tp.svg";
-import useScrollLock from "../../../hooks/useScrollLock";
 
 export default function Header() {
   return (
-      <header className={`${styles.header}`}>
-        <div className={styles.logoContainer}>
-          <TruthPost  />
-        </div>
-        <div className={styles.subtitleContainer}>
-          <div className={styles.subtitle}>Accurate and Relevant News</div>
-          <hr />
-          <div className={styles.subtitleDate}>{new Date().toUTCString().slice(0, 16)}</div>
-        </div>
-      </header>
+    <header className={`${styles.header}`}>
+      <div className={styles.logoContainer}>
+        <TruthPost />
+      </div>
+      <div className={styles.subtitleContainer}>
+        <div className={styles.subtitle}>Accurate and Relevant News</div>
+        <hr />
+        <div className={styles.subtitleDate}>{new Date().toUTCString().slice(0, 16)}</div>
+      </div>
+    </header>
   );
 }
