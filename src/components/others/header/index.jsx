9a23307fce09c47a15e7036dc6b2f95b7c0afcf9
Ferit Tunçer <ferit@cryptolab.net>
2023-05-18 15:10:21 +0100

    change header title

--- src/components/others/header/index.jsx
+++ src/components/others/header/index.jsx
@@ -4,15 +4,13 @@ import TruthPost from "jsx:/src/assets/tp.svg";
 
 export default function Header() {
   return (
-    <header className={`${styles.header}`}>
-      <div className={styles.logoContainer}>
-        <TruthPost />
-      </div>
-      <div className={styles.subtitleContainer}>
-        <div className={styles.subtitle}>Accurate and Relevant News</div>
-        <hr />
-        <div className={styles.subtitleDate}>{new Date().toUTCString().slice(0, 16)}</div>
-      </div>
-    </header>
+      <header className={`${styles.header}`}>
+        <h1>The Truth Post</h1>
+        <div className={styles.subtitleContainer}>
+          <div className={styles.subtitle}>Accurate and Relevant News</div>
+          <hr />
+          <div className={styles.subtitleDate}>{new Date().toUTCString().slice(0, 16)}</div>
+        </div>
+      </header>
   );
 }
