c847fb037791dd4e4d95e7386b36fdd4fd0e4273
gratestas <stasgrate@gmail.com>
2023-05-23 21:02:18 +0300

    chore: change button positions in ConfirmCreate

--- src/components/others/confirmCreate/index.jsx
+++ src/components/others/confirmCreate/index.jsx
@@ -1,36 +1,30 @@
 import Tag from "/src/components/presentational/tag";
 import * as styles from "./index.module.scss";
-import {constants} from "ethers";
+import { constants } from "ethers";
 import CustomButton from "/src/components/presentational/button";
 
-
-export default function ConfirmCreate({title, description, tags, bounty, categoryNo, handleCreate, handleGoBack}) {
-
-
+export default function ConfirmCreate({ title, description, tags, bounty, categoryNo, handleCreate, handleGoBack }) {
   return (
     <>
       <div className={styles.bountyAmount}>
-        Bounty:{" "}{bounty}{' '}{constants.EtherSymbol}
+        Bounty: {bounty} {constants.EtherSymbol}
       </div>
       <h1>{title}</h1>
-      <p className={styles.description}>
-        {description}
-      </p>
+      <p className={styles.description}>{description}</p>
       <div className={styles.containerTag}>
-        {tags.split(' ').map((tag, index) => <Tag key={'tag' + index}>{tag}</Tag>)}
+        {tags.split(" ").map((tag, index) => (
+          <Tag key={"tag" + index}>{tag}</Tag>
+        ))}
       </div>
 
-
-
       <div className={styles.buttons}>
+        <CustomButton type="button" modifiers="secondary" onClick={handleGoBack}>
+          Go Back And Edit
+        </CustomButton>
         <CustomButton type="button" onClick={handleCreate}>
           Publish
         </CustomButton>
-        <CustomButton type="button" modifiers='secondary' onClick={handleGoBack}>
-          Go Back And Edit
-        </CustomButton>
       </div>
-
     </>
   );
 }
