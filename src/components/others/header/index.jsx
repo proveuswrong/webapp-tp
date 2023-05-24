5e754a923a9a13a6fa8588e020726ae9d9edd9dc
gratestas <stasgrate@gmail.com>
2023-05-24 16:48:03 +0300

    feat: add landing logo into header

--- src/components/others/header/index.jsx
+++ src/components/others/header/index.jsx
@@ -1,10 +1,14 @@
 import * as styles from "./index.module.scss";
 
 import TruthPost from "jsx:/src/assets/tp.svg";
+import TruthPostLogo from "jsx:/src/assets/truth-post-logo.svg";
 
 export default function Header() {
   return (
       <header className={`${styles.header}`}>
+        <div style={{width: "50px"}}>
+          <TruthPostLogo/>
+        </div>
         <h1>The Truth Post</h1>
         <div className={styles.subtitleContainer}>
           <div className={styles.subtitle}>Accurate and Relevant News</div>
