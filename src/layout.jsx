9697221e558a64188f60d32bc5d950e7fc272938
gratestas <stasgrate@gmail.com>
2023-05-31 13:21:03 +0300

    refactor: use new router construction with createBrowserRouter

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,14 +1,21 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 import Header from "./components/others/header";
 import Navigation from "./components/others/navigation";
 import Footer from "./components/others/footer";
 
-export default function Layout({children}) {
+export default function Layout() {
   return (
     <>
-      <Header/>
-      <Navigation/>
-      <main>{children}</main>
-      <Footer/>
+      <Header />
+      <Navigation />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+      <ToastContainer position="bottom-right" newestOnTop autoClose={10000} />
     </>
   );
 }
