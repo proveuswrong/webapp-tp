876ccd2477dd38eb0fbe13aa7884b383e8b05cda
Ferit Tunçer <ferit@cryptolab.net>
2023-06-02 19:07:07 +0100

    refactor: put new logo to header

--- src/components/others/header/index.jsx
+++ src/components/others/header/index.jsx
@@ -1,11 +1,11 @@
 import * as styles from "./index.module.scss";
 
-import TruthPost from "jsx:/src/assets/tp.svg";
+import TruthPost from "jsx:/src/assets/logoBgTransparent.svg";
 
 export default function Header() {
   return (
       <header className={`${styles.header}`}>
-        <h1>The Truth Post</h1>
+        <TruthPost className={styles.logo}/>
         <div className={styles.subtitleContainer}>
           <div className={styles.subtitle}>Accurate and Relevant News</div>
           <hr />
