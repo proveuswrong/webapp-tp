d3373e957bd534b7855ef10f9092197d55fa4879
Ferit Tunçer <ferit@cryptolab.net>
2023-06-02 15:00:10 +0100

    refactor: total withdrawable text

--- src/components/presentational/cardCrowdfundingWithdrawal/index.jsx
+++ src/components/presentational/cardCrowdfundingWithdrawal/index.jsx
@@ -3,13 +3,14 @@ import * as styles from "./index.module.scss";
 import RewardSymbol from "jsx:/src/assets/crown.svg";
 import CustomButton from "../button";
 import { ethers } from "ethers";
+import EtherValue from "../EtherValue";
 
 export default function CardCrowdfundingWithdrawal(props) {
   console.log(props);
   return <div className={`${styles.cardCrowdfundingWithdrawal}`}>
     <RewardSymbol/>
     {props.contributed && props.withdrew && <span>You already withdrew.</span>}
-    {props.contributed && !props.withdrew && <span>Total Rewards: {props.amount || 0} {ethers.constants.EtherSymbol}</span>}
+    {props.contributed && !props.withdrew && <div className={styles.totalWithdrawable}>Total Withdrawable Amount <EtherValue value={props.amount || 0}/></div>}
     {!props.contributed && <span>You did not contribute the crowdfunding of the last dispute.</span>}
 
     <hr/>
