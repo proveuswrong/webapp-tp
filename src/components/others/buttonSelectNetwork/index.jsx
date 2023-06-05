1916b84dd5352e32d79168ebe5dc2a8c7eb9306c
gratestas <stasgrate@gmail.com>
2023-06-05 18:42:45 +0300

    refactor: apply required changes to comply with routing updates

--- src/components/others/buttonSelectNetwork/index.jsx
+++ src/components/others/buttonSelectNetwork/index.jsx
@@ -1,30 +1,19 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 
 import Select from "/src/components/presentational/select";
 import { EthereumContext, networkMap } from "../../../data/ethereumProvider";
 
 export default function ButtonSelectNetwork() {
-  const navigate = useNavigate();
-  const ethereumContext = useContext(EthereumContext);
+  const { switchAppChain } = useContext(EthereumContext);
 
-  const selectOptions = Object.entries(networkMap).map(([chainId, props], index) => ({
+  const selectOptions = Object.entries(networkMap).map(([chainId, props]) => ({
     value: chainId,
     label: props.shortname,
   }));
 
-  function handleOnChange(chainId) {
-    console.log({ chainId });
-    navigate("/" + chainId + "/");
-    // if (ethereumContext?.isProviderDetected)
-    //   ethereum.request({
-    //     method: "wallet_switchEthereumChain",
-    //     params: [{ chainId }],
-    //   });
-  }
   return (
     <div style={{ marginLeft: "10px" }}>
-      <Select options={selectOptions} placeholder={networkMap[ethereumContext?.chainId]?.shortname} onChange={handleOnChange} />
+      <Select options={selectOptions} placeholder={selectOptions[0].label} onChange={switchAppChain} />
     </div>
   );
 }
