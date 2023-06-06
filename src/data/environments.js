a225fc150e0637dec12122e96864bc56f170f219
Ferit Tunçer <ferit@cryptolab.net>
2023-06-06 14:16:04 +0100

    refactor: upgrade prod contract

--- src/data/environments.js
+++ src/data/environments.js
@@ -5,7 +5,7 @@ const environments = {
     networkMap: {
       "0x5": {
         contractInstances: {
-          "0x4FE73e57343931E713728d5dc334F7298666E980": {
+          "0x1B6B1381781fB6BA1ee271CCD9Fad9a1bb70fA24": {
             subgraph: {
               endpoint: "https://api.thegraph.com/subgraphs/name/proveuswrong/thetruthpost",
               queries: commonQueries
