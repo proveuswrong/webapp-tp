de5516fb7680b03c563bdac09b791385785c50c5
Ferit Tunçer <ferit@cryptolab.net>
2023-06-08 17:05:58 +0100

    refactor: conditional rendering in confirm form

--- src/components/others/confirmCreate/index.jsx
+++ src/components/others/confirmCreate/index.jsx
@@ -2,15 +2,18 @@ import Tag from "/src/components/presentational/tag";
 import * as styles from "./index.module.scss";
 import { constants } from "ethers";
 import CustomButton from "/src/components/presentational/button";
+import ReactMarkdown from "react-markdown";
 
-export default function ConfirmCreate({ title, description, tags, bounty, categoryNo, handleCreate, handleGoBack }) {
+
+export default function ConfirmCreate({ title, description, tags, bounty, categoryNo, format, handleCreate, handleGoBack }) {
   return (
     <>
       <div className={styles.bountyAmount}>
         Bounty: {bounty} {constants.EtherSymbol}
       </div>
       <h1>{title}</h1>
-      <p className={styles.description}>{description}</p>
+      {format == "markdown" ? <ReactMarkdown className={styles.description}>{description}</ReactMarkdown> : <p className={styles.description}>{description}</p>}
+
       <div className={styles.containerTag}>
         {tags.split(" ").map((tag, index) => (
           <Tag key={"tag" + index}>{tag}</Tag>
