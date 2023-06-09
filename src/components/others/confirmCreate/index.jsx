144114fa95839c2a86dbe5da38182bdcd3ecac8e
Ferit Tunçer <ferit@cryptolab.net>
2023-06-09 17:32:07 +0100

    feat: support KaTeX in Markdown

--- src/components/others/confirmCreate/index.jsx
+++ src/components/others/confirmCreate/index.jsx
@@ -3,21 +3,42 @@ import * as styles from "./index.module.scss";
 import { constants } from "ethers";
 import CustomButton from "/src/components/presentational/button";
 import ReactMarkdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 
-export default function ConfirmCreate({ title, description, tags, bounty, categoryNo, format, handleCreate, handleGoBack }) {
+
+export default function ConfirmCreate({
+  title,
+  description,
+  tags,
+  bounty,
+  categoryNo,
+  format,
+  handleCreate,
+  handleGoBack,
+}) {
   return (
     <>
       <div className={styles.bountyAmount}>
         Bounty: {bounty} {constants.EtherSymbol}
       </div>
       <h1>{title}</h1>
-      {format == "markdown" ? <ReactMarkdown className={styles.description}>{description}</ReactMarkdown> : <p className={styles.description}>{description}</p>}
+      {format == "markdown" ? (
+        <ReactMarkdown className={styles.description} remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+          {description}
+        </ReactMarkdown>
+      ) : (
+        <p className={styles.description}>{description}</p>
+      )}
 
       <div className={styles.containerTag}>
-        {tags.split(" ").filter((tag) => tag !="").map((tag, index) => (
-          <Tag key={"tag" + index}>{tag}</Tag>
-        ))}
+        {tags
+          .split(" ")
+          .filter((tag) => tag != "")
+          .map((tag, index) => (
+            <Tag key={"tag" + index}>{tag}</Tag>
+          ))}
       </div>
 
       <div className={styles.buttons}>
--- src/components/others/route_article/content/index.jsx
+++ src/components/others/route_article/content/index.jsx
@@ -3,6 +3,9 @@ import * as styles from "./index.module.scss";
 import Tag from "/src/components/presentational/tag";
 import Pill from "/src/components/presentational/pill";
 import ReactMarkdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import 'katex/dist/katex.min.css' // `rehype-katex` does not import the CSS for you
 
 export default function Content(props) {
   const { articleContent, articleStatus, fetchingArticleContent } = props;
@@ -18,7 +21,13 @@ export default function Content(props) {
         <Pill>{articleStatus}</Pill>
       </div>
 
-      {articleContent?.format == "markdown" ? <ReactMarkdown className={styles.description}>{articleContent?.description}</ReactMarkdown> : <p className={styles.description}>{articleContent?.description}</p>}
+      {articleContent?.format == "markdown" ? (
+        <ReactMarkdown className={styles.description} remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+          {articleContent?.description}
+        </ReactMarkdown>
+      ) : (
+        <p className={styles.description}>{articleContent?.description}</p>
+      )}
       <div className={styles.containerTag}>
         {articleContent?.tags?.split(" ").map((tag, index) => (
           <Tag key={index}>{tag}</Tag>
