8e8487689bdccf64fa538884fab306336d9b6d9e
gratestas <stasgrate@gmail.com>
2023-06-10 19:13:35 +0300

    refactor: update Account route loader

--- src/routes/account/index.jsx
+++ src/routes/account/index.jsx
@@ -3,9 +3,28 @@ import * as styles from "./index.module.scss";
 
 import ListArticles from "/src/components/others/listArticles";
 import { getArticlesByAuthor } from "/src/data/ethereumProvider";
+import { ipfsGateway } from "/src/utils/addToIPFS";
 
 export async function loader({ params }) {
-  return await getArticlesByAuthor(params.chain, params.id);
+  const articles = await getArticlesByAuthor(params.chain, params.id);
+  const fetchPromises = articles.map(async (article) => {
+    if (!article) return null;
+    try {
+      const response = await fetch(ipfsGateway + article?.articleID);
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      }
+      const { title, description } = await response.json();
+      article.title = title;
+      article.description = description;
+    } catch (error) {
+      console.error(error);
+      throw new Error(error.message);
+    }
+  });
+
+  await Promise.all(fetchPromises);
+  return articles;
 }
 
 export default function Account() {
