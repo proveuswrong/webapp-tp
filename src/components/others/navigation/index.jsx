b598abb6d5eb940e7e80156422685d6145dbf478
gratestas <stasgrate@gmail.com>
2023-06-12 19:42:21 +0300

    refactor: remove duplicated code in Navigation component

--- src/components/others/navigation/index.jsx
+++ src/components/others/navigation/index.jsx
@@ -1,22 +1,31 @@
 import { useContext, useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import * as styles from "./index.module.scss";
 
 import ButtonConnect from "/src/components/others/buttonConnect";
 import ButtonSelectNetwork from "/src/components/others/buttonSelectNetwork";
 import BurgerMenu from "../../presentational/burgerMenu";
 
-import { EthereumContext, networkMap } from "/src/data/ethereumProvider";
+import { EthereumContext } from "/src/data/ethereumProvider";
 import NotificationCenter from "../notificationCenter";
 
 import useScrollLock from "../../../hooks/useScrollLock";
 
 const DISABLED = true;
 
+const getLinks = (chainId, account) => [
+  { name: "Front Page", to: `${chainId}/`, display: true },
+  { name: "Report", to: `${chainId}/report`, display: account },
+  { name: "Account", to: `${chainId}/account/${account}`, display: account },
+  { name: "F.A.Q.", to: `faq`, display: true },
+  { name: "About", to: `about`, display: true },
+];
+
 export default function Navigation() {
-  const ethereumContext = useContext(EthereumContext);
+  const { chainId, accounts } = useContext(EthereumContext);
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [lockScroll, unlockScroll] = useScrollLock();
+  const params = useParams();
 
   const toggleMenu = () => setMenuOpen((prev) => !prev);
 
@@ -27,25 +36,13 @@ export default function Navigation() {
     };
   }, [isMenuOpen]);
 
-  const links = [
-    { name: "Front Page", to: `${ethereumContext?.chainId}/`, display: true },
-    { name: "Report", to: `${ethereumContext?.chainId}/report`, display: ethereumContext.accounts[0] },
-    {
-      name: "Account",
-      to: `${ethereumContext.chainId}/account/${ethereumContext?.accounts[0]}`,
-      display: ethereumContext.accounts[0],
-    },
-    { name: "F.A.Q.", to: `faq`, display: true },
-    { name: "About", to: `about`, display: true },
-  ];
-
   return (
     <>
       <nav className={`withBackground ${styles.navWrapper} ${isMenuOpen ? styles.fixedAtTop : ""} `}>
         <BurgerMenu isOpen={isMenuOpen} onClick={toggleMenu} />
         <div className={styles.nav}>
           <h2 hide="">Navigation</h2>
-          {links
+          {getLinks(chainId, accounts[0])
             .filter((link) => link.display)
             .map((link) => (
               <NavLink key={link.to} to={link.to}>
@@ -56,8 +53,8 @@ export default function Navigation() {
         {!isMenuOpen && (
           <div className={styles.navEthereum}>
             {!DISABLED && <NotificationCenter />} {/* TODO: unlock when UI design is ready */}
-            {ethereumContext?.chainId && ethereumContext?.isProviderDetected && <ButtonConnect />}
-            {ethereumContext?.chainId && <ButtonSelectNetwork />}
+            {params.chain && <ButtonConnect />}
+            {params.chain && <ButtonSelectNetwork />}
           </div>
         )}
       </nav>
@@ -67,29 +64,13 @@ export default function Navigation() {
 }
 
 function OverlayNav({ isMenuOpen, toggleMenu }) {
-  const ethereumContext = useContext(EthereumContext);
-
-  const links = [
-    { name: "Front Page", to: `${ethereumContext?.chainId || Object.keys(networkMap)[0]}/`, display: true },
-    {
-      name: "Report",
-      to: `${ethereumContext?.chainId || Object.keys(networkMap)[0]}/report`,
-      display: ethereumContext.accounts[0],
-    },
-    {
-      name: "Account",
-      to: `${ethereumContext?.chainId || Object.keys(networkMap)[0]}/account/${ethereumContext?.accounts[0]}`,
-      display: ethereumContext.accounts[0],
-    },
-    { name: "F.A.Q.", to: `faq/`, display: true },
-    { name: "About", to: `about/`, display: true },
-  ]; // TODO Remove duplication. (Had to duplicate because of Ethereum context dependency.)
+  const { chainId, accounts } = useContext(EthereumContext);
 
   return (
     <div className={`withBackground ${styles.overlay} ${isMenuOpen && styles.visible}`}>
       <nav className={`${styles.overlayNav} ${isMenuOpen && styles.visible}`}>
         <h2>Navigation</h2>
-        {links
+        {getLinks(chainId, accounts[0])
           .filter((link) => link.display)
           .map((link) => (
             <NavLink key={link.to} to={link.to} onClick={toggleMenu}>
