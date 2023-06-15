77da93b8408e476f1578d821fa2a3c694e912f4b
Ferit Tunçer <ferit@cryptolab.net>
2023-06-15 17:41:00 +0100

    refactor: replace some texts

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -16,13 +16,7 @@ export default function Home() {
             </div>
           </div>
           <div className={styles.text}>
-            Imagine a world where you can effortlessly consume distilled information, perfectly tailored to your
-            interests. With our decentralized newspaper, that dream becomes a reality. Imagine a world where you can
-            effortlessly consume distilled information, perfectly tailored to your interests. With our decentralized
-            newspaper, that dream becomes a reality. Imagine a world where you can effortlessly consume distilled
-            information, perfectly tailored to your interests. With our decentralized newspaper, that dream becomes a
-            reality. Imagine a world where you can effortlessly consume distilled information, perfectly tailored to
-            your interests. With our decentralized newspaper, that dream becomes a reality. No longer will you be b
+            The Truth Post is an innovative project designed to offer a platform where reporters, readers, and curators can engage in the reporting, reading, and curation of articles in a trustless environment. By leveraging cryptoeconomic techniques and decentralized dispute resolution, the platform aims to guarantee the provision of accurate and relevant news articles. Moreover, it offers reporters the opportunity to publish articles pseudonymously and earn rewards commensurate with their contributions’ impact.
           </div>
         </div>
       </section>
@@ -32,13 +26,7 @@ export default function Home() {
             <img src={bannerImage} alt="truthpost-banner" />
           </div>
           <div className={styles.text}>
-            Imagine a world where you can effortlessly consume distilled information, perfectly tailored to your
-            interests. With our decentralized newspaper, that dream becomes a reality. Imagine a world where you can
-            effortlessly consume distilled information, perfectly tailored to your interests. With our decentralized
-            newspaper, that dream becomes a reality. Imagine a world where you can effortlessly consume distilled
-            information, perfectly tailored to your interests. With our decentralized newspaper, that dream becomes a
-            reality. Imagine a world where you can effortlessly consume distilled information, perfectly tailored to
-            your interests. With our decentralized newspaper, that dream becomes a reality.
+            The Truth Post empowers users with a permissionless, pseudonymous, trustless, transparent, decentralized, autonomous, and fair ecosystem. With our innovative platform, anyone can publish, read, or curate news articles, ensuring an egalitarian space that rewards quality and impact.
           </div>
         </div>
       </section>
@@ -49,19 +37,13 @@ export default function Home() {
             <strong>Be Part of the Revolution:</strong> Discover the World's First Decentralized Newspaper
           </div>
           <div className={styles.firstParagraph}>
-            Imagine a world where you can effortlessly consume distilled information, perfectly tailored to your
-            interests. With our decentralized newspaper, that dream becomes a reality. No longer will you be bogged
-            dImagine a world where you can effortlessly consume distilled information, perfectly tailored to your
-            interests.
+            At The Truth Post, everyone has a role to play. Authors can freely express their narratives, readers can enjoy distilled information, and curators can maintain the quality and relevance of content. Each user role is intricately woven into our platform’s fabric, fostering an environment that rewards contributions based on their merit.
           </div>
           <div className={styles.imageWrapper}>
             <img src={WhatSectionImage} alt="truthpost-what-section" />
           </div>
           <div className={styles.secondParagraph}>
-            Imagine a world where you can effortlessly consume distilled information, perfectly tailored to your
-            interests. With our decentralized newspaper, that dream becomes a reality. No longer will you be bogged
-            dImagine a world where you can effortlessly consume distilled information, perfectly tailored to your
-            interests. With our decentralized newspaper, that dream becomes a reality. No longer will you be bogged d
+            We deploy cryptoeconomic incentives to cultivate a fair, quality-driven platform. Authors are rewarded based on the trust and relevance scores of their articles, incentivizing accurate and relevant news reporting. Curators, on the other hand, are motivated to challenge article accuracy and assign fair relevance scores with potential rewards and penalties.
           </div>
         </div>
       </section>
