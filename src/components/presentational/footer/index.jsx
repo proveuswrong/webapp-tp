e5f51978d63db0dfa7d01d2710b1f14b9ecbe337
Ferit Tunçer <ferit@cryptolab.net>
2023-06-23 14:51:16 +0100

    refactor: delete footer text

--- src/components/presentational/footer/index.jsx
+++ src/components/presentational/footer/index.jsx
@@ -6,30 +6,35 @@ import * as styles from "./index.module.scss";
 export default function Index() {
   return (
     <footer className={styles.footer}>
-      <span className={`ellipsis ${styles.containerText}`}>Work-in-progress. Expect missing or broken functionality and bugs.</span>
+      <span className={`ellipsis ${styles.containerText}`}></span>
       <div className={styles.containerSocialAndCopyright}>
-      <span className={styles.social}>
-        <a href="https://github.com/proveuswrong" target="_blank" rel="noopener noreferrer" title="GitHub Organization">
-          <span style={{display: "none"}}>Link to GitHub profile</span>
-          <Github aria-hidden="true" id="github"/>
+        <span className={styles.social}>
+          <a
+            href="https://github.com/proveuswrong"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="GitHub Organization"
+          >
+            <span style={{ display: "none" }}>Link to GitHub profile</span>
+            <Github aria-hidden="true" id="github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/prove-us-wrong/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="LinkedIn Company Page"
+          >
+            <span style={{ display: "none" }}>Link to LinkedIn profile</span>
+            <LinkedIn aria-hidden="true" id="linkedin" />
+          </a>
+          <a href="https://twitter.com/ProveUsWrongIO" target="_blank" rel="noopener noreferrer" title="Twitter Page">
+            <span style={{ display: "none" }}>Link to Twitter profile</span>
+            <Twitter aria-hidden="true" id="twitter" />
+          </a>
+        </span>
+        <a href="https://proveuswrong.io" target="_blank" rel="noopener noreferrer" className={styles.copyright}>
+          Copyright {new Date().getFullYear()} Prove Us Wrong
         </a>
-        <a
-          href="https://www.linkedin.com/company/prove-us-wrong/"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="LinkedIn Company Page"
-        >
-          <span style={{display: "none"}}>Link to LinkedIn profile</span>
-          <LinkedIn aria-hidden="true" id="linkedin"/>
-        </a>
-        <a href="https://twitter.com/ProveUsWrongIO" target="_blank" rel="noopener noreferrer" title="Twitter Page">
-          <span style={{display: "none"}}>Link to Twitter profile</span>
-          <Twitter aria-hidden="true" id="twitter"/>
-        </a>
-      </span>
-        <a href='https://proveuswrong.io' target='_blank' rel='noopener noreferrer'
-           className={styles.copyright}>Copyright {new Date().getFullYear()} Prove Us Wrong</a>
-
       </div>
     </footer>
   );
