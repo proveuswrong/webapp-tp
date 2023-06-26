04af576a6c1ea3f8bbf91c3924c34d4466dd68e5
gratestas <stasgrate@gmail.com>
2023-06-26 13:37:21 +0300

    feat: notify with msg to install Metamask if no provider detected

--- src/components/others/buttonConnect/index.jsx
+++ src/components/others/buttonConnect/index.jsx
@@ -1,48 +1,66 @@
-import React, { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { toast } from "react-toastify";
+
 import { EthereumContext, networkMap } from "../../../data/ethereumProvider";
 import CustomButton from "/src/components/presentational/button";
 
+const CONNECTION_REQUEST_MSG = (
+  <>
+    <div>Please install Metamask to connect to app</div>
+    <a href="https://metamask.io/download/" target="_blank">
+      Install
+    </a>
+  </>
+);
+
 export default function ButtonConnect() {
+  const ethereumContext = useContext(EthereumContext);
+
+  useEffect(() => {
+    if (!ethereumContext.isProviderDetected)
+      toast.warn(CONNECTION_REQUEST_MSG, {
+        autoClose: false,
+      });
+  }, []);
+
   return (
-    <EthereumContext.Consumer>
-      {(ethereumContext) => {
-        return (
-          <CustomButton
-            modifiers="small secondary"
-            id="buttonConnect"
-            disabled={ethereumContext?.awaitingUserPermission || !ethereumContext?.isDeployedOnThisChain}
-            onClick={() => {
-              if (ethereumContext?.accounts.length < 1) {
-                ethereumContext.requestAccounts().then(() => {
-                  ethereum.request({
-                    method: "wallet_switchEthereumChain",
-                    params: [{ chainId: ethereumContext.chainId }],
-                  });
-                });
-              } else {
-                console.log("There is a connected account already.");
-              }
-            }}
-          >
-            <a
-              href={
-                ethereumContext?.accounts[0] && ethereumContext.isDeployedOnThisChain
-                  ? networkMap[ethereumContext?.chainId].explorerURL(ethereumContext?.accounts[0])
-                  : undefined
-              }
-              target="_blank"
-              rel="noopener noreferrer"
-              key={ethereumContext?.accounts[0]}
-              className="blink"
-            >
-              {!ethereumContext?.accounts[0] && ethereumContext?.awaitingUserPermission && "Awaiting User Permission"}
-              {!ethereumContext?.accounts[0] && !ethereumContext?.awaitingUserPermission && "Connect Account"}
-              {ethereumContext?.accounts[0] &&
-                `${ethereumContext?.accounts[0]?.substring(0, 6)}...${ethereumContext?.accounts[0]?.slice(-4)}`}
-            </a>
-          </CustomButton>
-        );
+    <CustomButton
+      modifiers="small secondary"
+      id="buttonConnect"
+      disabled={
+        !ethereumContext.isProviderDetected ||
+        ethereumContext?.awaitingUserPermission ||
+        !ethereumContext?.isDeployedOnThisChain
+      }
+      onClick={() => {
+        if (ethereumContext?.accounts.length < 1) {
+          ethereumContext.requestAccounts().then(() => {
+            ethereum.request({
+              method: "wallet_switchEthereumChain",
+              params: [{ chainId: ethereumContext.chainId }],
+            });
+          });
+        } else {
+          console.log("There is a connected account already.");
+        }
       }}
-    </EthereumContext.Consumer>
+    >
+      <a
+        href={
+          ethereumContext?.accounts[0] && ethereumContext.isDeployedOnThisChain
+            ? networkMap[ethereumContext?.chainId].explorerURL(ethereumContext?.accounts[0])
+            : undefined
+        }
+        target="_blank"
+        rel="noopener noreferrer"
+        key={ethereumContext?.accounts[0]}
+        className="blink"
+      >
+        {!ethereumContext?.accounts[0] && ethereumContext?.awaitingUserPermission && "Awaiting User Permission"}
+        {!ethereumContext?.accounts[0] && !ethereumContext?.awaitingUserPermission && "Connect Account"}
+        {ethereumContext?.accounts[0] &&
+          `${ethereumContext?.accounts[0]?.substring(0, 6)}...${ethereumContext?.accounts[0]?.slice(-4)}`}
+      </a>
+    </CustomButton>
   );
 }
