9a5a92602759271cdf0f995ebddc0d585436cb87
gratestas <stasgrate@gmail.com>
2023-08-06 22:09:37 +0300

    refactor(progress-bar): add adaptive styling

--- src/components/presentational/progressBar/index.jsx
+++ src/components/presentational/progressBar/index.jsx
@@ -1,6 +1,16 @@
 import { Progress } from "antd";
 import * as styles from "./index.module.scss";
 
+import { getBreakpointValue } from "../../../utils/breakpoints";
+import { useMediaQuery } from "../../../hooks/useMediaQuery";
+
 export default function ProgressBar(props) {
-  return <Progress className={styles.progressBar} type="line" trailColor="transparent" status="active" {...props} />;
+  const breakpoint = getBreakpointValue("large");
+  const isCircular = useMediaQuery(`(max-width: ${breakpoint}px)`);
+
+  const type = isCircular ? "circle" : "line";
+  const strokeLinecap = isCircular ? "square" : "round";
+  const typeStyles = isCircular ? styles.progressBarCircular : styles.progressBar;
+
+  return <Progress className={typeStyles} type={type} strokeLinecap={strokeLinecap} {...props} />;
 }
