52574e2fd0360e886e687e7e702e123c6045b1fa
gratestas <stasgrate@gmail.com>
2024-09-13 22:25:38 +0300

    chore: update subgraph endpoint for Mainnet

--- src/data/environments.js
+++ src/data/environments.js
@@ -8,7 +8,7 @@ const environments = {
         deployments: {
           "0x87aade1067ed0276ec9bef6db8e17abe27a6b454": {
             subgraph: {
-              endpoint: "https://api.thegraph.com/subgraphs/name/proveuswrong/thetruthpost",
+              endpoint: "https://api.studio.thegraph.com/query/88965/thetruthpost/version/latest",
               queries: commonQueries,
             },
           },
@@ -34,7 +34,7 @@ const environments = {
         deployments: {
           "0x87aade1067ed0276ec9bef6db8e17abe27a6b454": {
             subgraph: {
-              endpoint: "https://api.thegraph.com/subgraphs/name/proveuswrong/thetruthpost",
+              endpoint: "https://api.studio.thegraph.com/query/88965/thetruthpost/version/latest",
               queries: commonQueries,
             },
           },
